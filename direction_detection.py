37b4f943570a30cfc7462bceafd507289342a220
MichaelZigray <59930528+MichaelZigray@users.noreply.github.com>
2023-03-04 17:28:10 -0700

    created direction file

---
+++ direction_detection.py
@@ -0,0 +1,13 @@
+
+# class screen:
+#     def __init__(self, width, height):
+#         self.width = width
+#         self.height = height
+#         self.center = (width/2, height/2) # MIGHT BE AN ISSUE WITH X AND Y SWAPPED
+
+# class eyeball:
+#     def __init__(self, center, tl, tr, bl, br, screen):
+#         self.center = center
+#         self.screen = screen
+
+#     def make_eye_map(self, tl, tr, bl, br):
